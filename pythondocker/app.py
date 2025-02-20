f6d61b639455ede999cb47e20d453809cfdd20e1
lu22111 <139249445+lu22111@users.noreply.github.com>
2025-02-20 15:32:40 -0600

    PROYECTOTO1

---
+++ pythondocker/app.py
@@ -0,0 +1,24 @@
+from flask import Flask , render_template
+import mysql.connector
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="db"
+    )
+    cursor = conn.cursor()
+    cursor.execute("select * from students")
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return render_template('index.html',students= result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
